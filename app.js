f4f1d6cf83afff52e102e2f92aa587a9b91294b8
kris.kensy <kriskensy@proton.me>
2026-05-05 21:28:14 +0200

    Add addImagetWatermarkToImage function

--- app.js
+++ app.js
@@ -14,3 +14,19 @@ const addTextWatermarkToImage = async function (inputFile, outputFile, text) {
 };
 
 addTextWatermarkToImage('./test.jpg', './test-with-watermark.jpg', 'Hello Kris');
+
+const addImageWatermarkToImage = async function (inputFile, outputFile, watermarkFile) {
+  const image = await Jimp.read(inputFile);
+  const watermark = (await Jimp.read(watermarkFile)).resize(500, Jimp.AUTO);
+  const positionX = image.getWidth() / 2 - watermark.getWidth() / 2;
+  const positionY = image.getHeight() / 2 - watermark.getHeight() / 2;
+
+  image.composite(watermark, positionX, positionY, {
+    mode: Jimp.BLEND_SOURCE_OVER,
+    opacitySource: 0.3,
+  });
+
+  await image.quality(100).writeAsync(outputFile);
+};
+
+addImageWatermarkToImage('./test.jpg', './test-with-watermark2.jpg', './logo.png');
