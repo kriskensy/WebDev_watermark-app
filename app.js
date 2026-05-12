afd1170bb3d1badec94351ac317ccfe6ca5f48ea
kris.kensy <kriskensy@proton.me>
2026-05-12 20:59:46 +0200

    Add try catch to both addWatermark funtions

--- app.js
+++ app.js
@@ -3,30 +3,50 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 const addTextWatermarkToImage = async function (inputFile, outputFile, text) {
-  const image = await Jimp.read(inputFile);
-  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
-  const textData = {
-    text,
-    alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-    alignmentY: Jimp. VERTICAL_ALIGN_MIDDLE,
-  }
+  try {
+    const image = await Jimp.read(inputFile);
+    const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
+    const textData = {
+      text,
+      alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+      alignmentY: Jimp. VERTICAL_ALIGN_MIDDLE,
+    }
 
-  image.print(font, 10, 10, textData, image.getWidth(), image.getHeight());
-  await image.quality(100).writeAsync(outputFile);
+    image.print(font, 10, 10, textData, image.getWidth(), image.getHeight());
+    await image.quality(100).writeAsync(outputFile);
+
+    console.log('Success!');
+  }
+  catch (error) {
+    console.log('Something went wrong... Try again!')
+  }
+  finally {
+    startApp();
+  }
 };
 
 const addImageWatermarkToImage = async function (inputFile, outputFile, watermarkFile) {
-  const image = await Jimp.read(inputFile);
-  const watermark = (await Jimp.read(watermarkFile)).resize(500, Jimp.AUTO);
-  const positionX = image.getWidth() / 2 - watermark.getWidth() / 2;
-  const positionY = image.getHeight() / 2 - watermark.getHeight() / 2;
+  try {
+    const image = await Jimp.read(inputFile);
+    const watermark = (await Jimp.read(watermarkFile)).resize(500, Jimp.AUTO);
+    const positionX = image.getWidth() / 2 - watermark.getWidth() / 2;
+    const positionY = image.getHeight() / 2 - watermark.getHeight() / 2;
 
-  image.composite(watermark, positionX, positionY, {
-    mode: Jimp.BLEND_SOURCE_OVER,
-    opacitySource: 0.3,
-  });
+    image.composite(watermark, positionX, positionY, {
+      mode: Jimp.BLEND_SOURCE_OVER,
+      opacitySource: 0.3,
+    });
 
-  await image.quality(100).writeAsync(outputFile);
+    await image.quality(100).writeAsync(outputFile);
+
+    console.log('Success!');
+  }
+  catch (error) {
+    console.log('Something went wrong... Try again!')
+  }
+  finally {
+    startApp();
+  }
 };
 
 const prepareOutputFilename = (filename) => {
